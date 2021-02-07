dfc415e5bc12d647d503416eab4fab37f80ac2ba
AirRunner <luca.vaio@efrei.net>
2021-02-07 15:50:13 +0100

    Add exercise 6 from Lab 2 (average computation)

---
+++ Lab2_ex6/Average.scala
@@ -0,0 +1,17 @@
+object Average {
+    def average(values: Iterator[Double]): Option[Double] = {
+        val sum = values.foldRight((0.0, 0)) {
+            (el, acc) => (el + acc._1, acc._2 + 1)
+        }
+        if (sum._2 == 0) { None }
+        else Some(sum._1 / sum._2)
+    }
+    def average2(values: Iterator[Double]): Option[Double] = {
+        if (values.hasNext) {
+            val sum = values.map((_, 1.0))
+                            .reduce((x, y) => (x._1 + y._1, x._2 + y._2))
+            Some(sum._1 / sum._2)
+        }
+        else None
+    }
+}
