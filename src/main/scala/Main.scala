a2d8c03c0a792dbaf817819f23c58cb88e1c7a50
AirRunner <luca.vaio@efrei.net>
2021-03-31 19:59:13 +0200

    Refactor project into separated services

--- src/main/scala/Main.scala
+++
@@ -1,9 +0,0 @@
-//import ReportGenerator.jsonReport
-import Kafka.kafkaConsume
-
-object Main {
-    def main(args: Array[String]): Unit = {
-        //val droneReport = jsonReport()
-        kafkaConsume("drone-reports")
-    }
-}
