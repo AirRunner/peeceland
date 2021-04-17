b3540be9201ffc7c1793962843e3508328b1ddcd
Stan <stanislas.kiesgen.de.richter@efrei.net>
2021-04-17 19:06:24 +0200

    Added spark context and csv import

--- services/data-analysis/src/main/scala/Main.scala
+++ services/data-analysis/src/main/scala/Main.scala
@@ -1,5 +1,13 @@
-object Main {
-    def main(args: Array[String]): Unit = {
-        
-    }
+import org.apache.spark.sql.SparkSession
+
+object SparkWordCount extends App {
+
+    val spark = SparkSession.builder
+      .master("local[*]")
+      .appName("Spark Load Csv")
+      .getOrCreate()
+
+
+    val df = spark.read.option("header",true).csv("resources/FakeCsv.csv")
+    df.printSchema()
 }
