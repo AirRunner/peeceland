f15e444068cfb42488fef47d8ad208856d71ae0c
GabrielPre <gabriel.precigout@efrei.net>
2021-04-19 23:11:23 +0200

    Mongospark object update

--- services/data-storage/src/main/scala/MongoSpark.scala
+++ services/data-storage/src/main/scala/MongoSpark.scala
@@ -11,14 +11,14 @@ object MongoSpark {
   def main(args: Array[String]): Unit = {
     //Start the Spark context
 
-    val mongoConnectionURL = "mongodb+srv://peaceland:9CmNkjVzR1e1xR0v@devcluster.4jen4.mongodb.net/?retryWrites=true&w=majority"
+    val mongoConnectionURI = "mongodb+srv://peaceland:9CmNkjVzR1e1xR0v@devcluster.4jen4.mongodb.net/?retryWrites=true&w=majority"
 
     val spark = SparkSession
       .builder()
       .master("local")
       .appName("MongoSpark")
-      .config("spark.mongodb.input.uri", mongoConnectionURL)
-      .config("spark.mongodb.output.uri", mongoConnectionURL)
+      .config("spark.mongodb.input.uri", mongoConnectionURI)
+      .config("spark.mongodb.output.uri", mongoConnectionURI)
       .getOrCreate()
 
     //Read data from MongoDB
