a49cc2c48601757654e7b0f180736213a728c93e
Stan <stanislas.kiesgen.de.richter@efrei.net>
2021-04-19 23:21:53 +0200

    Update main data analysis

--- services/data-analysis/src/main/scala/data/analysis/Main.scala
+++ services/data-analysis/src/main/scala/data/analysis/Main.scala
@@ -1,10 +1,15 @@
 package data.analysis
 
+import org.apache.log4j.BasicConfigurator
+
 object Main {
   def main(args: Array[String]): Unit = {
+    BasicConfigurator.configure()
+    //var df=CSV_import.loadCSV()
+    //df.printSchema()
 
-    var df=CSV_import.loadCSV()
-    df.printSchema()
+    val df = CsvFromS3.loadCSV()
 
+    Analysis1.analysis1(df)
   }
 }
